6121740b3c18bcd12a466ffdb8e89ac3b5d32f80
nattfodd <nattfodd.pp.ua@gmail.com>
2018-02-26 16:30:07 +0100

    WIP (constast experiments)

--- lib/despeck/commands/remove.rb
+++ lib/despeck/commands/remove.rb
@@ -31,18 +31,40 @@ module Despeck
         # the color we search for as a CIELAB coordinate
         # match = [54.64, 61.16, 51.76]
         # image = image.colourspace('b-w')
-        binding.pry
-        match = hex_to_rgb(color)
-        distance = image.dE76(image.new_from_image(match))
-        image = (distance > sensitivity).ifthenelse([255, 255, 255], image)
+        # match = hex_to_rgb(color)
+        # distance = image.dE76(image.new_from_image(match))
+        # image = (distance > sensitivity).ifthenelse([255, 255, 255], image)
 
-        # binding.pry
-        # image.more_const1(220)
+        # # binding.pry
+        # # image.more_const1(220)
+        image = grayscale_algorithm(image)
         image = image.colourspace('b-w')
+        # image = increase_contrast(image)
+        image = improve_black(image)
         image.write_to_file(output_file)#'output/01.jpg')
       end
 
       private
+
+      def grayscale_algorithm(image)
+        # TODO: Write a function to add adjust those params from color provided
+        #       or from `sensitivity` option
+        image.recomb([1.5, 0.03, 0.03]) 
+      end
+
+      def increase_contrast(bw_image)
+        # Works pretty bad
+        # bw_image.recomb([0.95])
+        # bw_image.linear([0.9], [0])
+        # bw_image.linear([1.2], [50])
+        bw_image.colourspace('lch') * [1, 100, 100] + [0, 0, 500]
+      end
+
+      def improve_black(image)
+        match = [0, 0, 0]
+        distance = image.dE76(image.new_from_image(match))
+        (distance < 80).ifthenelse([0, 0, 0], image)
+      end
 
       def hex_to_rgb(hex)
         hex = hex.gsub(/^#/, '')
