646f4bb9b9da232e8ecb3996dde875e2a37ceacd
nattfodd <nattfodd.pp.ua@gmail.com>
2018-02-27 17:10:54 +0100

    Basic watermarks removal by color added

--- lib/despeck/commands/remove.rb
+++ lib/despeck/commands/remove.rb
@@ -12,56 +12,61 @@ module Despeck
                Integer(s)
              end
 
-      parameter 'input file', 'Input file - either PDF or image',
+      option ['--apply-contrast'],
+             'apply_contrast',
+             'Increase contrast of output image?',
+             flag: true
+
+      option ['--improve-black'],
+             'improve_black',
+             'Replace all grayish pixels to true black?',
+             flag: true
+
+      option ['-c', '--color'],
+             'COLOR',
+             'Watermark primary HEX colour (example: FEFE7E)',
+             required: true
+
+      parameter 'input_file', 'Input file - either PDF or image',
                 attribute_name: :input_file
-      parameter 'output file', 'Output file (same format as input)',
+      parameter 'output_file', 'Output file (same format as input)',
                 attribute_name: :output_file
-      parameter 'color', 'Color to be removed (example: #e86751)',
-                attribute_name: :color
 
       def execute
-        # binding.pry
         image = Vips::Image.new_from_file(input_file)
-        binding.pry
+        return if ColourChecker.new(image: image).black_and_white?
 
-        # Preprocess: remove white noise from scanning
-        # distance = Vips::Image.sum(((image - [255, 255, 255]) ** 2).bandsplit) ** 0.5
-        # swap pixels more than 50 away for white
-        # image = (distance < sensitivity).ifthenelse([255, 255, 255], image)
-
-        # the color we search for as a CIELAB coordinate
-        # match = [54.64, 61.16, 51.76]
-        # image = image.colourspace('b-w')
-        # match = hex_to_rgb(color)
-        # distance = image.dE76(image.new_from_image(match))
-        # image = (distance > sensitivity).ifthenelse([255, 255, 255], image)
-
-        # # binding.pry
-        # # image.more_const1(220)
         image = grayscale_algorithm(image)
-        image = image.colourspace('b-w')
-        # image = increase_contrast(image)
-        image = improve_black(image)
-        image.write_to_file(output_file)#'output/01.jpg')
+        # image = image.colourspace('b-w')
+        image = increase_contrast(image) if apply_contrast
+        image = apply_black_improvement(image) if improve_black
+      ensure
+        image.write_to_file(output_file)
       end
 
       private
 
       def grayscale_algorithm(image)
-        # TODO: Write a function to add adjust those params from color provided
-        #       or from `sensitivity` option
-        image.recomb([1.2, 0.03, 0.03])
+        image.recomb(greyscale_params)
+      end
+
+      def greyscale_params
+        r, g, b = hex_to_rgb(color)
+        case [r, g, b].max
+        when r
+          [1.2, 0.03, 0.03]
+        when g
+          [0.03, 1.4, 0.03]
+        when b
+          [0.03, 0.03, 1.4]
+        end
       end
 
       def increase_contrast(bw_image)
-        # Works pretty bad
-        # bw_image.recomb([0.95])
-        # bw_image.linear([0.9], [0])
-        # bw_image.linear([1.2], [50])
         bw_image.colourspace('lch') * [1, 100, 100] + [0, 0, 500]
       end
 
-      def improve_black(image)
+      def apply_black_improvement(image)
         match = [0, 0, 0]
         distance = image.dE76(image.new_from_image(match))
         (distance < 80).ifthenelse([0, 0, 0], image)
@@ -70,18 +75,6 @@ module Despeck
       def hex_to_rgb(hex)
         hex = hex.gsub(/^#/, '')
         hex.chars.each_slice(2).map { |p| Integer("0x#{p.join}") }
-      end
-
-      def sigmoid(a, b)
-        lut = Vips::Image.identity(ushort: false)
-        max = lut.max
-        lut = lut.divide([max])
-        x = 1.0 / (1.0 + Math.exp(a * b))
-        y = 1.0 / (1.0 + Math.exp((a - 10) * b)) - x
-        z = lut.multiply([-1]).add([a]).multiply([b]).exp.add([1])
-        result = (z ** -1).subtract([x]).divide([y])
-        result = result.multiply([max])
-        result = result.cast(:uchar)
       end
     end
   end
