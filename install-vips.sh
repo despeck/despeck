a54cef3c57967e83017a4c7ed1386fa51df9874b
Zidni Mubarock <zidmubarock@gmail.com>
2020-09-09 00:54:39 +0700

    update install libvips to 8.7.4

--- install-vips.sh
+++ install-vips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 vips_site=https://github.com/jcupitt/libvips/releases/download
-version=$VIPS_VERSION_MAJOR.$VIPS_VERSION_MINOR.$VIPS_VERSION_MICRO
+version=$LIBVIPS_VERSION
 
 set -e
 
@@ -9,18 +9,23 @@ set -e
 # we could check the configure params as well I guess
 if [ -d "$HOME/vips/bin" ]; then
   installed_version=$($HOME/vips/bin/vips --version)
-  escaped_version="$VIPS_VERSION_MAJOR\.$VIPS_VERSION_MINOR\.$VIPS_VERSION_MICRO"
   echo "Need vips-$version"
   echo "Found $installed_version"
-  if [[ "$installed_version" =~ ^vips-$escaped_version ]]; then
+  if [[ "$installed_version" =~ ^vips-$version ]]; then
     echo "Using cached directory"
     exit 0
   fi
 fi
 
 rm -rf $HOME/vips
-wget $vips_site/v$version/vips-$version.tar.gz
+echo 'Downloading libvips source'
+wget $vips_site/v$version/vips-$version.tar.gz >/dev/null 2>&1
+echo 'Extracting'
 tar xf vips-$version.tar.gz
 cd vips-$version
-CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $*
-make && make install
+echo 'Configuring'
+CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $* >/dev/null 2>&1
+echo 'Make'
+make >/dev/null 2>&1
+echo 'Install'
+make install >/dev/null 2>&1
